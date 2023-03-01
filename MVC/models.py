ed026447d17570830c6d9ff43d3ecee1767a3440
Letícia <leticiaalaura@gmail.com>
2023-03-01 10:59:08 -0300

    Criação da Model

---
+++ MVC/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+class Categoria:
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+class Produtos:
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+
+class Estoque:
+    def __init__(self, produto: Produtos, quantidade):
+        self.produto = produto
+        self.quantidade = quantidade
+
+class Venda:
+    def __init__(self, itensVendidos: Produtos, quantidadeVendida, data = datetime.now()):
+        self.itensVendidos = itensVendidos
+        self.quantidadeVendida = quantidadeVendida
+        self.data = data
+
+class Fornecedor:
+    def __init__(self, nome, cnpj, contato, categoria = Categoria):
+        self.nome = nome
+        self.cnpj = cnpj
+        self.contato = contato
+        self.categoria = categoria
+
+class Pessoa:
+    def __init__(self, nome, cpf, contato, endereco):
+        self.nome = nome
+        self.cpf = cpf
+        self.contato = contato
+        self.endereco = endereco
+
+class Funcionario(Pessoa):
+    def __init__(self, cargo, nome, cpf, contato, endereco):
+        self.cargo = cargo
+        super(Funcionario, self).__init__(nome, cpf, contato, endereco)
