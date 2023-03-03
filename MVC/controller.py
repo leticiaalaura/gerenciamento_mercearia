9d6e72e2ac6bffcc3d3bc3142c714f38fdb7f27d
Letícia <leticiaalaura@gmail.com>
2023-03-03 17:18:17 -0300

    Cadastro de categoria

---
+++ MVC/controller.py
@@ -0,0 +1,18 @@
+from MVC.dao import DaoCategoria
+
+
+class ControllerCategoria:
+    def cadastraCategoria(self, novaCategoria):
+        existe = False
+        x = DaoCategoria.ler()
+        for i in x:
+            if i.categoria == novaCategoria:
+                existe = True
+        if not existe:
+            DaoCategoria.salvar(novaCategoria)
+            print('Categoria cadastrada com sucesso!')
+        else:
+            print('Categoria ja existente!')
+
+a = ControllerCategoria()
+a.cadastraCategoria('Frios')
